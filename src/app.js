3c1c03f6eae3f59fa659cb23ecc99909f03806d7
andvis <a.vistavkin@hotmail.com>
2023-04-27 17:05:50 +0200

    About111
    About111

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.get('/help', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About',
+        title: 'About111',
         name: 'DM'
     })
 })
